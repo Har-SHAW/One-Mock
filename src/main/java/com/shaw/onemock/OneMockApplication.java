a0f0990a5f8c602de288ccd6fbd68b216d92b14e
har5haw <harshavardhan.gadireddy@zemosolabs.com>
2022-04-28 17:07:46 +0530

    added folder in home directory

--- src/main/java/com/shaw/onemock/OneMockApplication.java
+++ src/main/java/com/shaw/onemock/OneMockApplication.java
@@ -3,10 +3,22 @@ package com.shaw.onemock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class OneMockApplication {
 
     public static void main(String[] args) {
+        try {
+            String userHomeDir = System.getProperty("user.home");
+            Path path = Paths.get(userHomeDir + "/.onemock");
+            Files.createDirectories(path);
+        } catch (IOException e) {
+            System.err.println("Failed to create directory!" + e.getMessage());
+        }
         SpringApplication.run(OneMockApplication.class, args);
     }
 
