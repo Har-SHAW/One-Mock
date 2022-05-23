0bf66ee2a9586dfaf2b28bc12efb2704216d7936
har-shaw <8888harsha4444@gmail.com>
2022-05-24 01:07:48 +0530

    removed old controllers

--- src/main/java/com/shaw/onemock/controllers/dashboard/CaptureController.java
+++ src/main/java/com/shaw/onemock/controllers/dashboard/CaptureController.java
@@ -1,46 +1,32 @@
 package com.shaw.onemock.controllers.dashboard;
 
 import com.shaw.onemock.constants.CaptureState;
-import com.shaw.onemock.constants.GlobalConstants;
-import com.shaw.onemock.dtos.requests.PartialRequestDto;
+import com.shaw.onemock.dtos.responses.SimpleResponse;
 import com.shaw.onemock.exceptions.RequestNotFound;
 import com.shaw.onemock.services.CaptureService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@Controller
-@RequestMapping("/dashboard/capture")
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/capture")
 public class CaptureController {
     @Autowired
-    private CaptureService captureService;
+    CaptureService captureService;
 
     @GetMapping("/toggle-capture")
-    public String toggleCapture() {
+    public SimpleResponse toggleCapture() {
         CaptureState.toggle();
-        return "redirect:/dashboard/capture";
+        return new SimpleResponse("success", true, 200);
     }
 
     @GetMapping
-    public String getCapture(Model model, @RequestParam(required = false, name = "id") Long id) throws RequestNotFound {
-        List<PartialRequestDto> partialRequestDtos = captureService.getAll();
-        model.addAttribute("requests", partialRequestDtos);
-        model.addAttribute("capture", CaptureState.getCapture());
-        if (partialRequestDtos.isEmpty()) {
-            model.addAttribute("empty", true);
-            return GlobalConstants.CAPTURE_PAGE;
-        } else {
-            model.addAttribute("empty", false);
-        }
+    public ResponseEntity<?> getCapture(Model model, @RequestParam(required = false, name = "id") Long id) throws RequestNotFound {
         if (id == null) {
-            id = partialRequestDtos.get(0).getId();
+            return ResponseEntity.ok(captureService.getAll());
         }
-        model.addAttribute("fullRequest", captureService.getOne(id));
-        return GlobalConstants.CAPTURE_PAGE;
+        return ResponseEntity.ok(captureService.getOne(id));
     }
 }
--- src/main/java/com/shaw/onemock/controllers/dashboard/MocksController.java
+++ src/main/java/com/shaw/onemock/controllers/dashboard/MocksController.java
@@ -1,84 +1,44 @@
 package com.shaw.onemock.controllers.dashboard;
 
-import com.shaw.onemock.constants.GlobalConstants;
-import com.shaw.onemock.dtos.mocks.CustomResponseDto;
 import com.shaw.onemock.dtos.mocks.MockRequestDto;
-import com.shaw.onemock.dtos.mocks.SimpleMockDto;
+import com.shaw.onemock.dtos.responses.SimpleResponse;
 import com.shaw.onemock.exceptions.MockAlreadyExist;
 import com.shaw.onemock.exceptions.MockRequestNotFound;
 import com.shaw.onemock.services.MockService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@Controller
-@RequestMapping("/dashboard/mocks")
+@RestController
+@RequestMapping("/api/mocks")
+@CrossOrigin("http://localhost:3000")
 public class MocksController {
     @Autowired
     private MockService mockService;
 
     @GetMapping
-    public String getMockPage(Model model, @RequestParam(required = false, name = "id") Long id) throws MockRequestNotFound {
-        List<SimpleMockDto> mocks = mockService.getAllMocks();
-        model.addAttribute("mocks", mocks);
-        if (mocks.isEmpty()) {
-            model.addAttribute("empty", true);
-            return GlobalConstants.MOCKS_PAGE;
-        }
+    public ResponseEntity<?> getFullMock(@RequestParam(name = "id", required = false) Long id) throws MockRequestNotFound {
         if (id == null) {
-            id = mocks.get(0).getId();
+            return ResponseEntity.ok().body(mockService.getAllMocks());
         }
-        model.addAttribute("fullMock", mockService.getFullMock(id));
-        model.addAttribute("empty", false);
-        return GlobalConstants.MOCKS_PAGE;
+        return ResponseEntity.ok().body(mockService.getFullMock(id));
     }
 
-    @GetMapping("/create")
-    public String createMock(Model model) {
-        MockRequestDto mockRequestDto = new MockRequestDto();
-        mockRequestDto.addCustomResponseDto(new CustomResponseDto());
-        model.addAttribute("mock", mockRequestDto);
-        model.addAttribute("methods", GlobalConstants.METHODS);
-        model.addAttribute("mode", "create");
-        return GlobalConstants.CREATE_MOCK_PAGE;
-    }
-
-    @GetMapping("/update")
-    public String updateMock(Model model, @RequestParam(name = "id") Long id) throws MockRequestNotFound {
-        MockRequestDto mockRequestDto = mockService.getFullMock(id);
-        if (mockRequestDto.getCustomResponseDtoSet().isEmpty()) {
-            mockRequestDto.getCustomResponseDtoSet().add(new CustomResponseDto());
-        }
-        model.addAttribute("mock", mockRequestDto);
-        model.addAttribute("methods", GlobalConstants.METHODS);
-        model.addAttribute("mode", "update");
-        return GlobalConstants.CREATE_MOCK_PAGE;
-    }
-
-    @PostMapping("/create")
-    public String addMock(@ModelAttribute("mock") MockRequestDto mockRequestDto, Model model) throws MockAlreadyExist {
-        System.out.println(mockRequestDto);
+    @PostMapping
+    public SimpleResponse addMock(@RequestBody MockRequestDto mockRequestDto) throws MockAlreadyExist {
         mockService.addMock(mockRequestDto);
-        model.addAttribute("methods", GlobalConstants.METHODS);
-        return "redirect:/dashboard/mocks";
+        return new SimpleResponse("success", true, 200);
     }
 
-    @PostMapping("/update")
-    public String updateMock(@ModelAttribute("mock") MockRequestDto mockRequestDto, Model model) throws MockRequestNotFound {
-        System.out.println(mockRequestDto);
-        mockService.updateMock(mockRequestDto, mockRequestDto.getId());
-        model.addAttribute("methods", GlobalConstants.METHODS);
-        return "redirect:/dashboard/mocks";
+    @PutMapping
+    public SimpleResponse updateMock(@RequestBody MockRequestDto mockRequestDto, @RequestParam(name = "id") Long id) throws MockRequestNotFound {
+        mockService.updateMock(mockRequestDto, id);
+        return new SimpleResponse("success", true, 200);
     }
 
-    @GetMapping("/delete")
-    public String deleteMock(Model model, @RequestParam(name = "id") Long mockId) {
+    @DeleteMapping
+    public SimpleResponse deleteMock(@RequestParam(name = "id") Long mockId) {
         mockService.deleteMock(mockId);
-        model.addAttribute("hasFullMock", false);
-        model.addAttribute("mocks", mockService.getAllMocks());
-        return "redirect:/dashboard/mocks";
+        return new SimpleResponse("success", true, 200);
     }
 }
